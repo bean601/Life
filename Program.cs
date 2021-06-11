4fe99f29bf2fa39788514c7042a121e8ffcf6636
Jeremy Bean <jeremy.david.bean@gmail.com>
2021-06-11 13:32:54 -0600

    it works!

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading;
 
 namespace life
 {
     public class Program
     {
-        private static bool[,] _currentGeneration = new bool[Console.WindowWidth, Console.WindowHeight];
-        private static bool[,] _nextGeneration = new bool[Console.WindowWidth, Console.WindowHeight];
+        private readonly static int _width = Console.WindowWidth;
+        private readonly static int _height = Console.WindowHeight;
+        private static bool[,] _currentGeneration = new bool[_width, _height];
+        private static bool[,] _nextGeneration = new bool[_width, _height];
 
         public class Cell
         {
@@ -27,7 +28,6 @@ namespace life
                 S = new Cell();
                 SW = new Cell();
                 W = new Cell();
-                Current = new Cell();
             }
 
             public Cell NW { get; set; }
@@ -38,9 +38,8 @@ namespace life
             public Cell S { get; set; }
             public Cell SW { get; set; }
             public Cell W { get; set; }
-            public Cell Current { get; set; }
 
-            public int NumberOfAliveNieghbords
+            public int NumberOfAliveNeighbors
             {
                 get
                 {
@@ -58,11 +57,12 @@ namespace life
             }
         }
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //Console.WriteLine(board.GetLength(0));
-            //Console.WriteLine(board.GetLength(1));
-            
+            _currentGeneration[9, 10] = true;
+            _currentGeneration[9, 11] = true;
+            _currentGeneration[9, 12] = true;
+
             while (true)
             {
                 DoLife();
@@ -82,14 +82,15 @@ namespace life
             {
                 for (int column = 0; column < _currentGeneration.GetLength(0); column++)
                 {
+                    var current = _currentGeneration[column, row];
                     var neighborhood = GetNeighborhood(row, column);
-                    neighborhood.Current.IsAlive = neighborhood.NumberOfAliveNieghbords > (neighborhood.Current.IsAlive ? 1 : 2);
 
-                    _nextGeneration[column, row] = neighborhood.Current.IsAlive;
+                    _nextGeneration[column, row] = neighborhood.NumberOfAliveNeighbors > (current ? 1 : 2);
                 }
             }
 
             _currentGeneration = _nextGeneration;
+            _nextGeneration = new bool[_width, _height];
         }
 
         private static void Render()
@@ -104,7 +105,7 @@ namespace life
                     if (!(row == 0 && column < 7))
                     {
                         Console.SetCursorPosition(column, row);
-                        Console.Write('#');
+                        Console.Write(_currentGeneration[column, row] ? '#' : ' ');
                     }
                 }
             }
@@ -121,22 +122,22 @@ namespace life
 
             // logic
             // nw
-            if (row - 1 > 0 && column - 1 > 0 && row - 1 >= _currentGeneration.GetLength(1) && column - 1 >= _currentGeneration.GetLength(0))
+            if (row - 1 > 0 && column - 1 > 0 && row - 1 < _currentGeneration.GetLength(1) && column - 1 < _currentGeneration.GetLength(0))
             {
                 neighborhood.NW.IsValid = true;
                 neighborhood.NW.IsAlive = _currentGeneration[column - 1, row - 1];
             }
             // n
-            if (row - 1 > 0 && row - 1 >= _currentGeneration.GetLength(1) && column >= _currentGeneration.GetLength(0))
+            if (row - 1 > 0 && row - 1 < _currentGeneration.GetLength(1) && column < _currentGeneration.GetLength(0))
             {
                 neighborhood.N.IsValid = true;
                 neighborhood.N.IsAlive = _currentGeneration[column, row - 1];
             }
             // ne
-            if (row + 1 < _currentGeneration.GetLength(1) && column + 1 < _currentGeneration.GetLength(0))
+            if (row - 1 > 0 && row - 1 < _currentGeneration.GetLength(1) && column + 1 < _currentGeneration.GetLength(0))
             {
                 neighborhood.NE.IsValid = true;
-                neighborhood.NE.IsAlive = _currentGeneration[column + 1, row + 1];
+                neighborhood.NE.IsAlive = _currentGeneration[column + 1, row - 1];
             }
             // e
             if (row < _currentGeneration.GetLength(1) && column + 1 < _currentGeneration.GetLength(0))
@@ -151,26 +152,23 @@ namespace life
                 neighborhood.SE.IsAlive = _currentGeneration[column + 1, row + 1];
             }
             // s
-            if (row <= _currentGeneration.GetLength(1) && column + 1 < _currentGeneration.GetLength(0))
+            if (row + 1 < _currentGeneration.GetLength(1) && column < _currentGeneration.GetLength(0))
             {
                 neighborhood.S.IsValid = true;
-                neighborhood.S.IsAlive = _currentGeneration[column + 1, row];
+                neighborhood.S.IsAlive = _currentGeneration[column, row + 1];
             }
             // sw
-            if (column - 1 > 0 && row - 1 > 0 && row + 1 <= _currentGeneration.GetLength(1) && column - 1 <= _currentGeneration.GetLength(0))
+            if (column - 1 > 0 && row + 1 < _currentGeneration.GetLength(1) && column - 1 < _currentGeneration.GetLength(0))
             {
                 neighborhood.SW.IsValid = true;
-                neighborhood.SW.IsAlive = _currentGeneration[column - 1, row - 1];
+                neighborhood.SW.IsAlive = _currentGeneration[column - 1, row + 1];
             }
             // w
-            if (column - 1 > 0 && row <= _currentGeneration.GetLength(1) && column - 1 <= _currentGeneration.GetLength(0))
+            if (column - 1 > 0 && row < _currentGeneration.GetLength(1) && column - 1 < _currentGeneration.GetLength(0))
             {
                 neighborhood.W.IsValid = true;
                 neighborhood.W.IsAlive = _currentGeneration[column - 1, row];
             }
-
-            neighborhood.Current.IsValid = true;
-            neighborhood.Current.IsAlive = _currentGeneration[column, row];
 
             return neighborhood;
         }
